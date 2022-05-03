77cc1aba4b2edc65ad0ec6bb34c500228cdda2b1
Mona <tigerzmona@yahoo.com>
2022-05-02 22:46:33 -0400

    homepage and swivel of arrow on button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/hi'
+import { HiArrowNarrowRight } from 'react-icons/hi'
 
 const Home = () => {
-  return (
-    <div className='w-full h-screen bg-[#0a192f]'>
-
-    </div>
-  )
+    return (
+        <div name='home' className='w-full h-screen bg-[#0a192f]'>
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+                <p className='text-white'>Hi, my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Mona</h1>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Blockchain & Fullstack Engineer.</h2>
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>My current focus is towards developing Web3 projects and
+                    fullstack dApps. I am also a technical writer in my spare time with a passion for all things technology, cats, and coffee ☕</p>
+                <div>
+                    <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-500 hover:border-purple-500'>
+                        View Work
+                        <span className='group-hover:rotate-90 duration-300'>
+                            <HiArrowNarrowRight className='ml-3' />
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Home
