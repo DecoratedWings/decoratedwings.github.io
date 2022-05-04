ae5189199671d4af64c26a8b2fc262ad57c99f9c
Mona <tigerzmona@yahoo.com>
2022-05-04 15:17:40 -0400

    homepage + video animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import SciFi from '../assets/scifi2.mp4';
 
 const Home = () => {
     return (
-        
-        <div name='home' className='w-full h-screen bg-[#0a192f]'>
+        // 9050c4 -Purple
+        //bg-[#020103] -> black
+        <div name='home' className='w-full h-screen '>
+            
             {/* Container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Mona</h1>
-                <h2 className='text-4xl sm:text-4xl font-bold text-[#8892b0]'>Blockchain & Fullstack Engineer</h2>
-                <p className='text-[#8892b0] py-4 max-w-[700px]'>My current focus is towards developing Web3 projects and
+                <h2 className='text-4xl sm:text-4xl font-bold text-[#a3aed0]'>Blockchain & Fullstack Engineer</h2>
+                <p className='text-[#a5b1d3] py-4 max-w-[700px]'>My current focus is towards developing Web3 projects and
                     fullstack dApps. I am also a technical writer in my spare time with a passion for all things technology, cats, and coffee ☕</p>
                 <div>
+
+    <video
+        autoPlay
+        
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={SciFi} type="video/mp4" />
+      </video>
+
                     <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-500 hover:border-purple-500'>
                         View Work
                         <span className='group-hover:rotate-90 duration-300'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,9 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    // #0a192f
+    // #582681 => purple
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#000000] text-gray-300'>
         <div>
           <a href="/">
             <img src={Logo} alt="Logo Image" style={{width:'50px'}}/>
