c68a0814d195cd4225546358ba6ab650ab189653
Mona <tigerzmona@yahoo.com>
2022-05-15 13:50:09 -0400

    added certifications

--- src/components/Certifications.jsx
+++ src/components/Certifications.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import ACE from '../assets/ACE.png'
+import ML from '../assets/ML.png'
+import C4 from '../assets/C4.jpg'
+
 
 const Certifications = () => {
   return (
@@ -7,6 +11,22 @@ const Certifications = () => {
           <div>
               <p className='text-4xl font-bold inline border-b-4 border-white '>Certifications</p>
           </div>
+
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-50 mx-auto' src={ACE} alt="GCP ACE Certifcation" />
+                  <p className='my-4'>GCP Associate Engineer</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-50 mx-auto' src={ML} alt="ML Certifcation" />
+                  <p className='my-4'>Stanford Machine Learning</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-50 mx-auto' src={C4} alt="Bitcoin Certifcation" />
+                  <p className='my-4'>Certified Bitcoin Professional</p>
+              </div>
+     
+              </div>
         </div>
     </div>
   )
