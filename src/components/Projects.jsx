0fb68d226b216f8b99c58134f5651c19ecf14873
Mona <tigerzmona@yahoo.com>
2022-05-15 14:25:39 -0400

    added links to projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,20 +15,28 @@ const Projects = () => {
 
           <div className='w-full grid grid-cols-2 lg:grid-cols-2 gap-8 text-center py-8'>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+              <a href="https://github.com/DecoratedWings/HapticDao">
                   <img className='w-100 mx-auto' src={HD} alt="Haptic Dao" />
+              </a>
                   <p className='my-4'>Haptic DAO: Chainlink 2022 Hackathon</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <a href="https://www.youtube.com/watch?v=L1PI3k5F-n8">
                   <img className='w-100 mx-auto' src={GC} alt="Giving Chain" />
+                </a>
                   <p className='my-4'>Giving Chain Blockchain NFT Donation Platform</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <a hre="https://www.youtube.com/watch?v=PWTTX4p6caI"> 
                   <img className='w-100 mx-auto' src={Singularity} alt="Singulariy" />
+                  </a>
                   <p className='my-4'>Winner for NY Deutsche Bank Internal Hackathon</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <a href="https://domain-starter-project.monarassouli.repl.co/">
                   <img className='w-100 mx-auto' src={domain} alt="Lit Domains" />
-                  <p className='my-4'>Domain Minting Dapp on Polygon: Lit Domains</p>
+                </a>
+                <p className='my-4'>Domain Minting Dapp on Polygon: Lit Domains</p>
               </div>
      
               </div>
